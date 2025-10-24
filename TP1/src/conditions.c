b304d0f5fcdbe8bd272eac16f1b2d981bb856417
aZna3 <anae.jubenot@gmail.com>
2025-10-24 11:55:49 +0200

    Update conditions.c

---
+++ TP1/src/conditions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int somme = 0; 
+    int i;
+
+    for (i = 1; i <= 1000; i++) {
+        if (i % 5 == 0 || i % 7 == 0) {
+            somme += i;
+        }
+        if (somme > 5000) {
+            break;
+        }
+    }
+    printf("somme : ", somme);
+
+    return 0;
+}
