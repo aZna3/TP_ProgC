69c9401d49d7e9ae3b8a3eebd5cff7d45d400acc
aZna3 <anae.jubenot@gmail.com>
2025-10-24 11:42:27 +0200

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int compteur = 5; 
+
+    if (compteur >= 10) {
+        return 1;
+    }
+
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (i == 1 || i == compteur) {
+                printf("* ");
+            } else if (j == 1 || j == i) {
+                printf("* ");
+            } else if (i % 2 == 0) {
+                printf("# ");
+            } else {
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
