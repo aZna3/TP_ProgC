6125635bbaa0d1d4589e50f399c8a3cfa6d2d36c
aZna3 <anae.jubenot@gmail.com>
2025-12-02 10:50:22 +0100

    Update etudiant.c

---
+++ TP2/src/etudiant.c
@@ -0,0 +1,17 @@
+int main() {
+
+    char noms[5][30] = {
+        "Jubénot", "Martin", "Isidore", "Marie-christ", "Buttant"
+    };
+
+    char prenoms[5][30] = {
+        "Anaé", "Karlotta", "Mathéo", "Wilfred", "Stéphane"
+    };
+
+    char adresses[5][50] = {
+        "12 Rue des martyrs",
+        "8 rue Pierre de Coubertin",
+        "25 Boulevard Général de Gaulle",
+        "4 Rue des Champs-Elysées",
+        "19 Rue Pontoise"
+    };
